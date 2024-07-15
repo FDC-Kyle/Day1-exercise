a194f6954c2f6780086ad9fa8ae4de728d09f440
FDC-Kyle <fdc.kylev@gmail.com>
2024-07-15 13:16:04 +0800

    Initial Commit

---
+++ Day1-exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day1-exercise
+//
+//  Created by FDC-KYLE-NC-WEB on 7/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
