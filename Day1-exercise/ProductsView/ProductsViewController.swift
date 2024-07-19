cebc760da62e68b05636b33f227064d4e7af3d67
FDC-Kyle <fdc.kylev@gmail.com>
2024-07-19 17:43:35 +0800

    cleaned code

--- Day1-exercise/ProductsView/ProductsViewController.swift
+++ Day1-exercise/ProductsView/ProductsViewController.swift
@@ -14,7 +14,7 @@ struct Product{
 
 class ProductsViewController: UIViewController{
     @IBOutlet weak var collectionView: UICollectionView!
-    private let viewModel = ProductsViewModel()
+
     var products: [Product] = [
         Product(name:"The Hunted", image: "hunter" ),
         Product(name:"Twilight Vampires", image: "vampire" ),
@@ -128,7 +128,7 @@ class ProductsViewController: UIViewController{
     ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewModel.fetchProducts()
+
         
         let nib = UINib(nibName: "ProductCell", bundle: nil)
         self.collectionView.register(nib, forCellWithReuseIdentifier: "ProductCell")
